3aa2ee9e99600a48a8bc98e8b7ffb1f916af8271
djinodissingar <dissingardjino@icloud.com>
2022-04-19 13:44:36 +0200

    Initial Commit

---
+++ chatApp/chatAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  chatAppApp.swift
+//  chatApp
+//
+//  Created by Djino Dissingar on 19/04/2022.
+//
+
+import SwiftUI
+
+@main
+struct chatAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
